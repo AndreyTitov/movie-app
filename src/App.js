bef684a43b91ab2cd845366315d6f66e02927867
Andrey Titov <andrey.titov@rock-west.com>
2022-11-09 12:50:36 +0200

    changed broser router basename

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import ScrollToTop from 'common/components/Navigator/SrollTop';
 
 function App () {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={`${window.local.href}${window.location.pathname}` || ''}>
       <Suspense fallback={<Loader/>}>
         <ScrollToTop />
         <Switch>
